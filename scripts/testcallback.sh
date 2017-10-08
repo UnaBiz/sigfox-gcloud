9e8f77de70fbdcf1d6a1987e46d6bcce6eb1d160
lupyuen-unabiz <ly.lee@unabiz.com>
2017-10-09 00:24:39 +0800

    Added test callback script

---
+++ scripts/testcallback.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Test the Sigfox Callback by sending a series of HTTP requests to simulate
+# POSTing of messages. Good for measuring response time.
+
+# Set the GCLOUD_PROJECT environment variable from .env
+export GCLOUD_PROJECT
+. .env
+
+# Send 10 test requests.
+for i in {1..10}
+do
+  # Wait 10 seconds between tests.
+  sleep 10
+
+  # wget command was exported from Postman. Remove all "\r\n" from body-data.
+  /usr/bin/time --format "\n***** %e Seconds *****\n" \
+    wget --verbose \
+    --method POST \
+    --header 'content-type: application/json' \
+    --header 'cache-control: no-cache' \
+    --body-data '{"device":"2C30EA",  "data":"b0513801a421f0019405a500",  "time":"1507396971",  "duplicate":"false",  "snr":"18.86",  "station":"1D44",  "avgSnr":"15.54",  "lat":"1",  "lng":"104",  "rssi":"-123.00",  "seqNumber":"1501",  "ack":"false",  "longPolling":"false"}' \
+    --output-document \
+    - https://us-central1-${GCLOUD_PROJECT}.cloudfunctions.net/sigfoxCallback
+
+done
--- sigfoxCallback/deploy.sh
+++ sigfoxCallback/deploy.sh
@@ -3,6 +3,8 @@
 name=sigfoxCallback
 trigger=--trigger-http
 topic=
+# Test with a larger memory size and timeout.
+export options="--memory=1024MB --timeout=500"
 
 ./scripts/functiondeploy.sh ${name}   ${name} ${trigger} ${topic}
 #./scripts/functiondeploy.sh ${name}01 ${name} ${trigger} ${topic}
