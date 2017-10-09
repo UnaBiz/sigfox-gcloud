8e72fd596d70d93309e7d8c20b514a21e2bd2def
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2017-10-09 16:47:25 +0800

    Log timestamp and operation to Google Cloud

--- index.js
+++ index.js
@@ -259,7 +259,7 @@ function deferLog(req, action, para0, record, now, loggingLog) { /* eslint-disab
           last: (para.err || para.result) ? true : false,
         };
         operationCache[operationid] = now;
-        const timestamp = new Date(now).toISOString();
+        const timestamp = new Date(now).toISOString().replace('Z', '000000Z');
         console.log({ now, timestamp });
         const metadata = {
           timestamp,
